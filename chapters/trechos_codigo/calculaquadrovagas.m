829950788adcfe00e7b7c1d75dfbde3bb65f1a59
Daniel Severo Estrázulas <estrazulas@users.noreply.github.com>
2019-10-08 15:45:46 -0300

    correções de texto - solicitadas pelo orientador - início

--- chapters/trechos_codigo/calculaquadrovagas.m
+++ chapters/trechos_codigo/calculaquadrovagas.m
@@ -1,29 +1,21 @@
-function calcula_vagasAcoesAfirmativas($totalVagas, $percentualNegros, $percentualEscolaPublica, $percentualPPIIBGE)
-{
+function calcula_vagasAcoesAfirmativas($totalVagas, $percentualNegros, $percentualEscolaPublica, $percentualPPIIBGE){
     $vagas = array();
-    
     $multEscolaPublica = $percentualEscolaPublica/100;
     $multPPIIBGE = $percentualPPIIBGE/100;
-    
     // vagas escola publica
     $vagas['AAEP'] = ceil($totalVagas * $multEscolaPublica);
     // restante para ampla concorrencia
     $vagas['CLAG'] = $totalVagas - $vagas['AAEP'];
-    
     // vagas escola publica renda < 1.5 SM
     $vagas['AAEPRI'] = ceil($vagas['AAEP'] * 0.5);
     // restante para escola publica renda > 1.5 SM
     $vagas['AAEPRS'] = $vagas['AAEP'] - $vagas['AAEPRI'];
-    
     // vagas escola publica renda < 1.5 SM - PPI
     $vagas['AAEPRIPPI'] = ceil($vagas['AAEPRI'] * $multPPIIBGE);
     // restante para escola publica renda < 1.5 SM - NPPI
     $vagas['AAEPRINPPI'] = $vagas['AAEPRI'] - $vagas['AAEPRIPPI'];
-    
     // vagas escola publica renda > 1.5 SM - PPI
     $vagas['AAEPRSPPI'] = ceil($vagas['AAEPRS'] * $multPPIIBGE);
     // restante para escola publica renda > 1.5 SM - NPPI
     $vagas['AAEPRSNPPI'] = $vagas['AAEPRS'] - $vagas['AAEPRSPPI'];
-    
-    return $vagas;
-}
+    return $vagas;}
